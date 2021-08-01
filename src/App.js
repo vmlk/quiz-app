e243dc1e3899705fcc9ba55b2100e1571717a937
vimalkwh <vimalkwh05@gmail.com>
2021-08-01 18:39:20 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const questions = [
+    {
+      questionText: "What is the capital of India?",
+      answerOptions: [
+        { answerText: "Delhi", isCorrect: true },
+        { answerText: "Lucknow", isCorrect: false },
+        { answerText: "Mumbai", isCorrect: false },
+        { answerText: "Chandigarh", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What is the national animal of India?",
+      answerOptions: [
+        { answerText: "Lion", isCorrect: false },
+        { answerText: "Tiger", isCorrect: true },
+        { answerText: "Cow", isCorrect: false },
+        { answerText: "Elephant", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What is the national flower of India?",
+      answerOptions: [
+        { answerText: "Rose", isCorrect: false },
+        { answerText: "Lotus", isCorrect: true },
+        { answerText: "Sunflower", isCorrect: false },
+        { answerText: "Tulip", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What is the national game of India?",
+      answerOptions: [
+        { answerText: "Cricket", isCorrect: false },
+        { answerText: "Basketball", isCorrect: false },
+        { answerText: "Footbal", isCorrect: false },
+        { answerText: "Hockey", isCorrect: true },
+      ],
+    },
+  ];
+
+  const handleButton = (isCorrect) => {
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+    isCorrect && setScore(score + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!showScore && (
+        <div className="app__container">
+          <div className="question__container">
+            <h2 className="question__text">
+              {questions[currentQuestion].questionText}
+            </h2>
+          </div>
+          <div className="second__container">
+            <div className="question__tracker">
+              <p>Question</p>
+              <h1>
+                {currentQuestion + 1}/{questions.length}
+              </h1>
+            </div>
+            <div className="question__option">
+              {questions[currentQuestion].answerOptions.map((answerTexts) => {
+                return (
+                  <button
+                    className="answer__btn"
+                    onClick={() => handleButton(answerTexts.isCorrect)}
+                  >
+                    {answerTexts.answerText}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      )}
+      {showScore && (
+        <div className="app__container">
+          <h1 className="score">
+            You scored <span>{score}</span> out of {questions.length}
+          </h1>
+          <button
+            className="answer__btn"
+            onClick={() => window.location.reload()}
+          >
+            Reattempt
+          </button>
+        </div>
+      )}
     </div>
   );
 }
